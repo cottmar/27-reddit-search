6570774c852dda7b479deaf86afff35b9da3697d
cara ottmar <cara.ottmar@gmail.com>
2018-05-22 15:06:51 -0700

    rendered boxes for text and number, added button.

--- src/main.js
+++ src/main.js
@@ -1,164 +1,126 @@
 import React from 'react';
 import { render as reactDomRender } from 'react-dom'; 
 import superagent from 'superagent';
-import './style/main.scss';
+import './styles/main.scss';
 
 const apiUrl = 'http://reddit.com/r/';
 
-class PokemonSearchForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pokeName: '',
-    };
-
-    this.handlePokemonNameChange = this.handlePokemonNameChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handlePokemonNameChange(event) {
-    this.setState({ pokeName: event.target.value });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.props.pokemonSelect(this.state.pokeName);
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input 
-          type="text"
-          name="pokemonName"
-          placeholder="Search for a Pokemon"
-          value={this.state.pokeName}
-          onChange={this.handlePokemonNameChange}
-        />
-      </form>
-    );
-  }
-}
+// App Component
+// should contain all of the application state
+// should contain methods for modifying the application state
+// the state should have a topics array for holding the results of the search
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pokemonLookup: {},
-      pokemonSelected: null,
-      pokemonNameError: null,
+      redditLookup: {},
     };
+  }
+  render() {
+    return (
+      <RedditSearchForm/>
+    );
+  }
+}
 
-    this.pokemonSelect = this.pokemonSelect.bind(this);
-    this.renderAbilitiesList = this.renderAbilitiesList.bind(this);
+
+// SearchForm Component
+// onSubmit the form should make a request to reddit
+// it should make a get request to http://reddit.com/r/${searchFormBoard}.json?limit=${searchFormLimit}
+// on success it should pass the results to the application state
+// on failure it should add a class to the form called error and turn the form's inputs borders red
+
+class RedditSearchForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redditForum: '',
+      redditForumLimit: null,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleForumChange = this.handleForumChange.bind(this);
+    this.handleLimitChange = this.handleLimitChange.bind(this);
   }
 
-  // built-in lifecycle hook from React, is invoked immediately after updating occurs
-  componentDidUpdate() {
-    console.log('__UPDATE STATE__', this.state);
+  handleForumChange(event) {
+    this.setState({ redditForum: event.target.value }, () => {
+      console.log(this.state);
+    });
   }
 
-  // built-in lifecycle hook from React, invoked after component is rendered on the page 
-  componentDidMount() {
-    if (localStorage.pokemonLookup) {
-      try {
-        const pokemonLookup = JSON.parse(localStorage.pokemonLookup);
-        return this.setState({ pokemonLookup });
-        // return undefined;
-      } catch (err) {
-        return console.error(err);
-      }
-    } else {
-      return superagent.get(apiUrl)
-        .then((response) => {
-          console.log(response);
-          const pokemonLookup = response.body.results.reduce((dict, result) => {
-            dict[result.name] = result.url;
-            return dict;
-          }, {});
-
-          try {
-            localStorage.pokemonLookup = JSON.stringify(pokemonLookup);
-            this.setState({ pokemonLookup });
-          } catch (err) {
-            console.error(err);
-          }
-        })
-        .catch(console.error);
-    }
+  handleLimitChange(event) {
+    this.setState({ redditForumLimit: event.target.value }, () => {
+      console.log(this.state);
+    });
   }
 
-  pokemonSelect(name) {
-    if (!this.state.pokemonLookup[name]) {
+  handleSubmit(event) {
+    event.preventDefault();
+    // this.props.redditForumSel(this.state.redditForum);
+  }
+  render() {
+    return (
+    <form onSubmit={this.handleSubmit}>
+      <input 
+        type='text'
+        placeholder="Search for a Reddit forum"
+        value={this.state.redditForum}
+        onChange={this.handleForumChange}
+      />
+      <input 
+        type='number'
+        placeholder="How many forum boards would you like?"
+        min='0'
+        max='100'
+        value={this.state.redditForumLimit}
+        onChange={this.handleLimitChange}
+      />
+      <button onClick= { this.handleForumChange }>Click here</button>
+    </form>
+    );
+  }
+  redditSelect(name) {
+    if (!this.state.redditLookup[name]) {
       this.setState({
-        pokemonSelected: null,
-        pokemonNameError: name,
+        redditSelected: null,
+        redditForumError: name,
       });
     } else {
-      return superagent.get(this.state.pokemonLookup[name])
+      return superagent.get(this.state.redditLookup[name])
         .then((response) => {
           this.setState({
-            pokemonSelected: response.body,
-            pokemonNameError: null,
+            redditSelected: response.body,
+            redditForumError: null,
           });
         })
         .catch(console.error);
     }
     return undefined;
   }
-
-  renderAbilitiesList(pokemon) {
-    return (
-      <ul>
-        { pokemon.abilities.map((item, index) => {
-          return (
-            <li key={index}>
-              <p>{item.ability.name}</p>
-            </li>
-          );
-        })}
-      </ul>
-    );
-  }
-
-
-  render() {
-    return (
-      <section>
-        <h1>Pokemon Form Demo</h1>
-        <PokemonSearchForm 
-          pokemonSelect={this.pokemonSelect}
-        />
-        { 
-          this.state.pokemonNameError ? 
-            <div>
-              <h2 className="error">
-                { `"${this.state.pokemonNameError}"`} does not exist.
-                Please make another request.
-              </h2>
-            </div> : 
-            <div>
-               {
-                 this.state.pokemonSelected ? 
-                 <div>
-                   <div>
-                     <img src={this.state.pokemonSelected.sprites.front_default} />
-                   </div>
-                   <h2>Selected: {this.state.pokemonSelected.name}</h2>
-                   <h3>Abilities:</h3>
-                   { this.renderAbilitiesList(this.state.pokemonSelected)}
-                 </div> :
-                 <div>
-                   Please make a request to see pokemon data.
-                 </div>
-               }
-            </div>
-        }
-
-      </section>
-    );
-  }
 }
+
+// SearchResultList Component
+// Should inherit all search results through props
+// This component does not need to have its own state
+// If there are topics in the application state it should display an unordered list
+// Each list item in the unordered list should contain the following
+// an anchor tag with a href to the topic.url
+// inside the anchor a heading tag with the topic.title
+// inside the anchor a p tag with the number of topic.ups
+
+// class RedditSearchResultList extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       redditSearchResult: IDKMYBFFJILL,
+//     };
+//   }
+// }
+
+
+
+
 
 const container = document.createElement('div');
 document.body.appendChild(container);
